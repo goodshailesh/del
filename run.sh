ce09af31249f6ab4135ee02e61761a512b7737c6
Soni Yadav <sy05dec@gmail.com>
2017-01-15 22:57:58 +1300

    Romote Script Added

--- run.sh
+++ run.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
-touch SAM_MISSILE
-echo $@ > SAM_MISSILE
-ls -l .
+if [ "$1" == '' ]; then
+	echo "Commit SHA missing"
+        echo "Usage: $0 [SHA] [IPADDR]"
+	exit
+fi
+if [ "$2" == '' ]; then
+	echo "Docker-Farm IP Addr missing"
+        echo "Usage: $0 [SHA] [IPADDR]"
+	exit
+fi
+export SHA=$1
+export DOCKER_FARM_IP_ADDR=$2
+ssh soni@"$DOCKER_FARM_IP_ADDR" -i /home/soni/.ssh/id_rsa.pem << EOF
+mkdir $SHA
+cd $SHA
+git init
+#git remote add origin git@github.com:goodshailesh/nodejs-docker-local-dev-env
+git remote add origin git@github.com:goodshailesh/del
+git pull origin master
+git checkout $SHA > /dev/null 2>&1
+export IHOME_PATH=`pwd`
+echo $IHOME_PATH
+
+EOF
